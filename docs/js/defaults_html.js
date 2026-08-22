85ff99cc678f272e1b62065956512c9cb53034cd
Mitch Halloran <smhalloran.career@gmail.com>
2026-08-21 22:35:37 -0700

    created 3A Home page to test for Canvas iframe use

--- docs/js/defaults_html.js
+++ docs/js/defaults_html.js
@@ -1,15 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("title").appendChild(document.createTextNode(document.querySelector('title').textContent));
-    const faviconAttributes = [
-        { rel: "icon", type: "image/x-icon", href: "favicon.ico", },
+    const linkElemAttributes = [
+        { rel: "icon", type: "image/x-icon", href: "favicon.ico" },
+        { rel: "shortcut-icon", type: "image/x-icon", href: "/chemistry/favicon.ico" },
         { rel: "icon", type: "image/png", sizes: "16x16", href: "favicon-16.png" },
         { rel: "icon", type: "image/png", sizes: "32x32", href: "favicon-32.png" },
         { rel: "apple-touch-icon", sizes: "180x180", href: "apple-touch-icon.png" },
-        { rel: "icon", type: "image/png", sizes: "192x192", href: "android-icon-192.png" }
+        { rel: "icon", type: "image/png", sizes: "192x192", href: "android-icon-192.png" },
+        { rel: "stylesheet", href: "css/std.css" },
+        { rel: "stylesheet", href: "css/compact.css" },
+        { rel: "stylesheet", href: "css/chem.css" }
     ];
+    const scriptElemAttributes = [];
     const headElem = document.getElementsByTagName("head")[0];
     let attribValue;
-    for (const linkElemData of faviconAttributes) {
+    for (const linkElemData of linkElemAttributes) {
         const linkElem = document.createElement("link");
         headElem.appendChild(linkElem);
         for (const attrib in linkElemData)
--- docs/src/html/defaults_html.ts
+++ docs/src/html/defaults_html.ts
@@ -1,6 +1,10 @@
-import type {
-   HTMLLinkElemAttributes
-} from "../types.js";
+import type { HTMLLinkElemAttributes } from "./defaults_html-types";
+
+
+// 2. Declaring a standalone global variable
+declare const APP_VERSION: string;
+
+
 
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -8,16 +12,26 @@ document.addEventListener("DOMContentLoaded", () => {
       document.querySelector('title')!.textContent
    ));
 
-   const faviconAttributes: HTMLLinkElemAttributes[] = [
-      {rel: "icon", type:"image/x-icon", href:"favicon.ico", },
+   const linkElemAttributes: HTMLLinkElemAttributes[] = [
+      {rel: "icon", type:"image/x-icon", href:"favicon.ico" },
+      {rel:"shortcut-icon", type:"image/x-icon", href:"/chemistry/favicon.ico"},
       {rel:"icon", type:"image/png", sizes:"16x16", href:"favicon-16.png"},
       {rel:"icon", type:"image/png", sizes:"32x32", href:"favicon-32.png"},
       {rel:"apple-touch-icon", sizes:"180x180", href:"apple-touch-icon.png"},
-      {rel:"icon", type:"image/png", sizes:"192x192", href:"android-icon-192.png"}
+      {rel:"icon", type:"image/png", sizes:"192x192", href:"android-icon-192.png"},
+
+      {rel:"stylesheet", href:"css/std.css"},
+      {rel:"stylesheet", href:"css/compact.css"},
+      {rel:"stylesheet", href:"css/chem.css"}
+
    ];
+
+
+   const scriptElemAttributes = [];
+
    const headElem = document.getElementsByTagName("head")[0];
    let attribValue;
-   for (const linkElemData of faviconAttributes) {
+   for (const linkElemData of linkElemAttributes) {
       const linkElem = document.createElement("link");
       headElem.appendChild(linkElem);
       for (const attrib in linkElemData)
